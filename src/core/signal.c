0804b2263f3dcc7e53def395ae4604af0919ee9b
octalide <ethayng@gmail.com>
2026-06-10 22:36:59 -0400

    fix(core): restart syscalls interrupted by handled signals

--- src/core/signal.c
+++ src/core/signal.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE  // SA_RESTART under -std=c17
+
 #include "core/signal.h"
 
 #include <signal.h>
@@ -12,6 +14,9 @@ void ap_signal_install(int sig, void (*handler)(int))
 #else
     struct sigaction sa = {0};
     sa.sa_handler = handler;
+    // restart interrupted syscalls so the shutdown signal doesn't fail
+    // an in-flight blocking read/write on a worker thread.
+    sa.sa_flags = SA_RESTART;
     sigemptyset(&sa.sa_mask);
     sigaction(sig, &sa, NULL);
 #endif
